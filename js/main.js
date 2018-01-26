5cbd4edc5d0d62c3900bb1f45d25edf49cc8b67c
w4n9hu1 <wanghui89757@gmail.com>
2018-01-26 13:35:30 +0800

    1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    $.get("http://180.168.142.114:826/api/blog",
-        function (data, textStatus, jqXHR) {
-            console.log(data);
-        },
-    );
+    // $.get("http://180.168.142.114:826/api/blog",
+    //     function (data, textStatus, jqXHR) {
+    //         console.log(data);
+    //     },
+    // );
 });
